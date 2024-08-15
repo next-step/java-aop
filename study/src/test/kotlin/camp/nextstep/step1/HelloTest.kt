00c00efcfbe6d2ac2ed5a740fa85155209562d57
jinyoung <jinyoungchoi95@gmail.com>
2024-08-16 02:28:09 +0900

    test: Enhancer 직접 사용 테스트 추가

--- study/src/test/kotlin/camp/nextstep/step1/HelloTest.kt
+++ study/src/test/kotlin/camp/nextstep/step1/HelloTest.kt
@@ -4,6 +4,7 @@ import camp.nextstep.aop.CglibAopProxy
 import camp.nextstep.aop.JdkDynamicAopProxy
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import net.sf.cglib.proxy.Enhancer
 import java.lang.reflect.Proxy
 
 class HelloTest : FunSpec({
@@ -36,10 +37,20 @@ class HelloTest : FunSpec({
             val cglibAopProxy = CglibAopProxy()
             cglibAopProxy.addAdvice(UpperCaseMethodInterceptor())
             cglibAopProxy.setTargetClass(HelloTarget::class.java)
-            val hello = cglibAopProxy.proxy as HelloTarget
+            val helloTarget = cglibAopProxy.proxy as HelloTarget
 
-            val actual = hello.sayThankYou("jin young")
+            val actual = helloTarget.sayThankYou("jin young")
             actual shouldBe "THANK YOU JIN YOUNG"
+        }
+
+        test("use Enhancer") {
+            val enhancer = Enhancer()
+            enhancer.setSuperclass(HelloTarget::class.java)
+            enhancer.setCallback(UpperCaseMethodInterceptor())
+            val helloTarget = enhancer.create() as HelloTarget
+
+            val actual = helloTarget.sayHello("jin young")
+            actual shouldBe "HELLO JIN YOUNG"
         }
     }
 })
