c540d1c5f793c5c899198fbbbd015cdbd831a286
junseo <djawnstj44@naver.com>
2024-08-17 23:13:20 +0900

    feat: 미션을 위한 Hello 인터페이스, 구현 클래스 생성

---
+++ study/src/test/java/camp/nextstep/HelloTarget.java
@@ -0,0 +1,18 @@
+package camp.nextstep;
+
+public class HelloTarget implements Hello {
+    @Override
+    public String sayHello(final String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHi(final String name) {
+        return "Hi " + name;
+    }
+
+    @Override
+    public String sayThankYou(final String name) {
+        return "Thank You " + name;
+    }
+}
