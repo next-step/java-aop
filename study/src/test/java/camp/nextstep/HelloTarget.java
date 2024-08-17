a4e100c372787d385a68987a5910e09ecfbfb568
junseo <djawnstj44@naver.com>
2024-08-17 23:15:29 +0900

    feat: 핑퐁 메서드 추가

--- study/src/test/java/camp/nextstep/HelloTarget.java
+++ study/src/test/java/camp/nextstep/HelloTarget.java
@@ -15,4 +15,9 @@ public class HelloTarget implements Hello {
     public String sayThankYou(final String name) {
         return "Thank You " + name;
     }
+
+    @Override
+    public String pingPong(final String name) {
+        return "pingPong " + name;
+    }
 }
