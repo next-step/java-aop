d318746a34a8597fbc50e99dac5eaa9df9877a34
junseo <djawnstj44@naver.com>
2024-08-17 23:14:06 +0900

    test: 기능 요구사항 2 - CGLIB Proxy

---
+++ study/src/test/java/camp/nextstep/CglibProxyTest.java
@@ -0,0 +1,36 @@
+package camp.nextstep;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class CglibProxyTest {
+
+    @DisplayName("구현 클래스만 이용해 모든 메서드의 반환 값을 대문자로 변환 한다")
+    @Test
+    public void upperCase() throws Exception {
+        // given
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloTarget.class);
+        enhancer.setCallback((MethodInterceptor) (o, method, objects, methodProxy) -> methodProxy.invokeSuper(o, objects).toString().toUpperCase());
+        final HelloTarget helloTarget = (HelloTarget) enhancer.create();
+
+        final String name = "name";
+
+        // when
+        final String helloActual = helloTarget.sayHello(name);
+        final String hiActual = helloTarget.sayHi(name);
+        final String thankYouActual = helloTarget.sayThankYou(name);
+
+        // then
+        assertAll(
+                () -> assertThat(helloActual).isEqualTo("HELLO NAME"),
+                () -> assertThat(hiActual).isEqualTo("HI NAME"),
+                () -> assertThat(thankYouActual).isEqualTo("THANK YOU NAME")
+        );
+    }
+}
