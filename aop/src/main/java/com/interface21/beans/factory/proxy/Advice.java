526ed0a69f04bc3d1e43626cd1785bf02446240c
jinyoung <jinyoungchoi95@gmail.com>
2024-08-19 00:03:59 +0900

    feat: Advice, JoinPoint 인터페이스 추가

---
+++ aop/src/main/java/com/interface21/beans/factory/proxy/Advice.java
@@ -0,0 +1,6 @@
+package com.interface21.beans.factory.proxy;
+
+public interface Advice {
+
+    Object invoke(JoinPoint joinPoint);
+}
---
+++ aop/src/main/java/com/interface21/beans/factory/proxy/JoinPoint.java
@@ -0,0 +1,7 @@
+package com.interface21.beans.factory.proxy;
+
+@FunctionalInterface
+public interface JoinPoint {
+
+    Object proceed();
+}
