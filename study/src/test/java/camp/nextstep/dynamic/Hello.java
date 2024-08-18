f4bd7fd886d067497d5767e0f84df01aa7850191
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-18 18:34:26 +0900

    feat : hello 객체 생성

---
+++ study/src/test/java/camp/nextstep/dynamic/Hello.java
@@ -0,0 +1,8 @@
+package camp.nextstep.dynamic;
+
+interface Hello {
+    String sayHello(String name);
+    String sayHi(String name);
+    String sayThankYou(String name);
+    String pingpong(String test);
+}
---
+++ study/src/test/java/camp/nextstep/dynamic/HelloTarget.java
@@ -0,0 +1,23 @@
+package camp.nextstep.dynamic;
+
+class HelloTarget implements Hello {
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return "Hi " + name;
+    }
+
+    @Override
+    public String sayThankYou(String name) {
+        return "Thank You " + name;
+    }
+
+    @Override
+    public String pingpong(String test) {
+        return "Pong " + test;
+    }
+}
