e8e310a549d96a5bcc7dc6f75a4eabfe951567f3
jinyoung <jinyoungchoi95@gmail.com>
2024-08-19 00:16:21 +0900

    fix: 패키지 수정

---
+++ aop/src/main/java/com/interface21/beans/factory/proxy/ProxyFactoryBean.java
@@ -0,0 +1,20 @@
+package com.interface21.beans.factory.proxy;
+
+import com.interface21.beans.factory.FactoryBean;
+import net.sf.cglib.proxy.Enhancer;
+
+public class ProxyFactoryBean<T> implements FactoryBean<T> {
+
+    private final Target<T> target;
+    private final Advisor advisor;
+
+    public ProxyFactoryBean(Target<T> target, Advisor advisor) {
+        this.target = target;
+        this.advisor = advisor;
+    }
+
+    @Override
+    public T getObject() {
+        return (T) Enhancer.create(target.getType(), advisor.createMethodInterceptor());
+    }
+}
--- aop/src/test/java/com/interface21/beans/factory/proxy/TargetTest.java
+++ aop/src/test/java/com/interface21/beans/factory/proxy/TargetTest.java
@@ -3,7 +3,7 @@ package com.interface21.beans.factory.proxy;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 class TargetTest {
 
     @Test
