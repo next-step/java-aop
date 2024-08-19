b503c18a4118553152269c5f3bb225d50da6eadb
JUNSEO <djawnstj44@naver.com>
2024-08-20 08:18:03 +0900

    test: TruePointcut 테스트

---
+++ aop/src/test/java/com/interface21/beans/factory/proxy/TruePointcutTest.java
@@ -0,0 +1,24 @@
+package com.interface21.beans.factory.proxy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class TruePointcutTest {
+
+    @DisplayName("matches 메서드의 반환 값으로 항상 true 를 반환 한다")
+    @Test
+    public void matchesTrue() throws Exception {
+        // given
+        final TruePointcut truePointcut = new TruePointcut();
+
+        // when
+        final boolean actual = truePointcut.matches(mock(), Object.class);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+}
