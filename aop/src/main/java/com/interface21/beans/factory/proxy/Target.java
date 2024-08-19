f16dcff1f8d4205f2c9460ae5a8a75ac0ff2e726
JUNSEO <djawnstj44@naver.com>
2024-08-20 08:18:24 +0900

    feat: Target 클래스 생성

---
+++ aop/src/main/java/com/interface21/beans/factory/proxy/Target.java
@@ -0,0 +1,14 @@
+package com.interface21.beans.factory.proxy;
+
+public class Target<T> {
+
+    private final Class<T> targetClass;
+
+    public Target(final Class<T> targetClass) {
+        this.targetClass = targetClass;
+    }
+
+    public Class<?> getTargetClass() {
+        return this.targetClass;
+    }
+}
