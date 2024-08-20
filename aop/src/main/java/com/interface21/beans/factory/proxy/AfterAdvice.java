6d26f1c95289024aaed9207671a4ebe34a6ad73e
jinyoung <jinyoungchoi95@gmail.com>
2024-08-20 23:15:39 +0900

    feat: AfterAdvice invoke fun to final

--- aop/src/main/java/com/interface21/beans/factory/proxy/AfterAdvice.java
+++ aop/src/main/java/com/interface21/beans/factory/proxy/AfterAdvice.java
@@ -7,7 +7,7 @@ public abstract class AfterAdvice implements Advice {
     abstract void afterReturning(Object returnValue, Method method, Object[] args, JoinPoint joinPoint);
 
     @Override
-    public Object invoke(JoinPoint joinPoint) throws Throwable {
+    public final Object invoke(JoinPoint joinPoint) throws Throwable {
         Object result = joinPoint.proceed();
         afterReturning(result, joinPoint.getMethod(), joinPoint.getArguments(), joinPoint);
         return result;
