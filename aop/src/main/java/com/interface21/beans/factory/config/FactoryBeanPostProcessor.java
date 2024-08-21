27c4ca0f299c0f464877f728c84bd1bba3603a1e
jinyoung <jinyoungchoi95@gmail.com>
2024-08-22 00:57:03 +0900

    feat: BeanPostProcessor accept 추가 및 FactoryBeanPostProcessor 구현/테스트

--- aop/src/main/java/com/interface21/beans/factory/config/FactoryBeanPostProcessor.java
+++ aop/src/main/java/com/interface21/beans/factory/config/FactoryBeanPostProcessor.java
@@ -4,6 +4,11 @@ import com.interface21.beans.factory.FactoryBean;
 
 public class FactoryBeanPostProcessor implements BeanPostProcessor {
     @Override
+    public boolean accept(Object bean) {
+        return bean instanceof FactoryBean<?>;
+    }
+
+    @Override
     public Object postInitialization(Object bean) {
         return ((FactoryBean<?>) bean).getObject();
     }
---
+++ aop/src/test/java/com/interface21/beans/factory/config/FactoryBeanPostProcessorTest.java
@@ -0,0 +1,49 @@
+package com.interface21.beans.factory.config;
+
+import com.interface21.beans.factory.proxy.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FactoryBeanPostProcessorTest {
+    
+    private final FactoryBeanPostProcessor beanPostProcessor = new FactoryBeanPostProcessor();
+
+    @Test
+    void FactoryBean에_대해서_지원한다() {
+        Target<Hello> target = new Target<>(Hello.class);
+        Advisor advisor = new Advisor(
+                method -> method.getName().startsWith("say"),
+                new UpperCaseAdvice()
+        );
+        ProxyFactoryBean<Hello> factoryBean = new ProxyFactoryBean<>(target, advisor);
+
+        boolean actual = beanPostProcessor.accept(factoryBean);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void FactoryBean이_아닌_bean은_지원하지_않는다() {
+        boolean actual = beanPostProcessor.accept(new Hello());
+        assertThat(actual).isFalse();
+    }
+
+    public static class Hello {
+
+        public String sayHello(String name) {
+            return "say " + name;
+        }
+
+        public String pingPong(String name) {
+            return "pingPong " + name;
+        }
+    }
+
+    private static class UpperCaseAdvice implements Advice {
+
+        @Override
+        public Object invoke(JoinPoint joinPoint) throws Throwable {
+            return ((String) joinPoint.proceed()).toUpperCase();
+        }
+    }
+}
