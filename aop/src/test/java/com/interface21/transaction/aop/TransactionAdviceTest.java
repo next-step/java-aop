becb86930b275a8e7712cdf4177576d85b254936
jinyoung <jinyoungchoi95@gmail.com>
2024-08-21 22:59:26 +0900

    test: joinPoint 실행 검증하도록 테스트 수정

--- aop/src/test/java/com/interface21/transaction/aop/TransactionAdviceTest.java
+++ aop/src/test/java/com/interface21/transaction/aop/TransactionAdviceTest.java
@@ -1,31 +1,20 @@
 package com.interface21.transaction.aop;
 
 import com.interface21.beans.factory.proxy.JoinPoint;
-import com.interface21.beans.factory.proxy.SimpleJoinPoint;
 import com.interface21.dao.DataAccessException;
 import com.interface21.transaction.PlatformTransactionManager;
-import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class TransactionAdviceTest {
 
     private final PlatformTransactionManager transactionManager = mock(PlatformTransactionManager.class);
-    private final JoinPoint joinPoint = new SimpleJoinPoint(
-            new Simple(),
-            Simple.class.getMethod("say"),
-            new Object[0],
-            MethodProxy.create(Simple.class, Simple.class, "()Ljava/lang/String;", "say", "say")
-    );
-
-    TransactionAdviceTest() throws NoSuchMethodException {
-    }
+    private final JoinPoint joinPoint = mock(JoinPoint.class);
 
     @BeforeEach
     void setUp() {
@@ -35,20 +24,16 @@ class TransactionAdviceTest {
     }
 
     @Test
-    void Transaction_실행_시_예외가_발생하면_rollback한다() {
+    void Transaction_실행_시_예외가_발생하면_rollback한다() throws Throwable {
         TransactionAdvice transactionAdvice = new TransactionAdvice(transactionManager);
+        when(joinPoint.proceed()).thenThrow(new RuntimeException("예외발생"));
 
         assertThatThrownBy(() -> transactionAdvice.invoke(joinPoint))
                 .isInstanceOf(DataAccessException.class);
 
-        InOrder inOrder = Mockito.inOrder(transactionManager);
+        InOrder inOrder = Mockito.inOrder(joinPoint, transactionManager);
         inOrder.verify(transactionManager).begin();
+        inOrder.verify(joinPoint).proceed();
         inOrder.verify(transactionManager).rollback();
-    }
-
-    private static class Simple {
-        public String say() {
-            return "say";
-        }
     }
 }
