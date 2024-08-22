73c6cf6c9e4a67bb8b1019f27e868fa79b0ceae6
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-22 13:33:15 +0900

    test : advice 테스트 작성

---
+++ aop/src/test/java/com/interface21/framework/CglibAopProxyTest.java
@@ -0,0 +1,96 @@
+package com.interface21.framework;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("CglibAopProxy 테스트")
+class CglibAopProxyTest {
+
+    private CglibAopProxy cglibAopProxy;
+
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Advised advised = new Advised(HelloService.class);
+        cglibAopProxy = new CglibAopProxy(advised);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @DisplayName("프록시 타입의 클래스를 이름을 확인한다.")
+    @Test
+    void testGetProxyClass() {
+        // given
+        Object proxy = cglibAopProxy.getProxy();
+
+        // when
+        Class<?> proxyClass = proxy.getClass();
+
+        // then
+        assertEquals("com.interface21.framework.CglibAopProxyTest$HelloService$$EnhancerByCGLIB$$8ffef10d", proxyClass.getName());
+    }
+
+    @DisplayName("프록시 객체를 생성하고 메서드를 호출한다.")
+    @Test
+    void testGetProxy() {
+        // given
+        HelloService proxy = (HelloService) cglibAopProxy.getProxy();
+
+        // when
+        String result = proxy.sayHello("World");
+
+        // then
+        assertEquals("Hello, World", result);
+    }
+
+    @DisplayName("등록된 advice를 통해 메서드 호출 전/후에 로직을 수행한다.")
+    @Test
+    void testGetProxyWithAdvice() {
+        // given
+        Advised advised = new Advised(HelloService.class);
+        advised.addAdvisor(new Advisor((method) -> true, new UpperCaseAdvice()));
+        advised.addAdvisor(new Advisor((method) -> true, new LoggingAdvice()));
+        CglibAopProxy cglibAopProxy = new CglibAopProxy(advised);
+        HelloService proxy = (HelloService) cglibAopProxy.getProxy();
+
+        // when
+        String result = proxy.sayHello("World");
+
+        // then
+        assertEquals("Method name: sayHello\n", outputStreamCaptor.toString());
+        assertEquals("HELLO, WORLD", result);
+    }
+
+    public static class HelloService {
+        public String sayHello(String name) {
+            return "Hello, " + name;
+        }
+    }
+
+    public static class UpperCaseAdvice implements AfterReturningAdvice {
+        @Override
+        public void invoke(MethodInvocation invocation) throws Throwable {
+            String result = (String) invocation.getReturnValue();
+            invocation.setReturnValue(result.toUpperCase());
+        }
+    }
+
+    public static class LoggingAdvice implements BeforeAdvice {
+        @Override
+        public void invoke(MethodInvocation invocation) throws Throwable {
+            System.out.println("Method name: " + invocation.getMethod().getName());
+        }
+    }
+}
--- study/src/test/java/camp/nextstep/cglib/CglibProxyTest.java
+++ study/src/test/java/camp/nextstep/cglib/CglibProxyTest.java
@@ -1,5 +1,10 @@
 package camp.nextstep.cglib;
 
+import com.interface21.beans.factory.ProxyFactoryBean;
+import com.interface21.framework.Advised;
+import com.interface21.framework.Advisor;
+import com.interface21.framework.AfterReturningAdvice;
+import com.interface21.framework.MethodInvocation;
 import net.sf.cglib.proxy.Enhancer;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
@@ -8,7 +13,7 @@ import org.junit.jupiter.api.Test;
 @DisplayName("CGLIB Proxy 테스트")
 class CglibProxyTest {
 
-    @DisplayName("프록시 객체를 생성하고 메서드를 호출한다.")
+    @DisplayName("say로 시작하는 메서드의 반환 값을 대문자로 변환한다.")
     @Test
     void sayHello() {
         Enhancer enhancer = new Enhancer();
@@ -20,5 +25,36 @@ class CglibProxyTest {
         assertThat(proxiedHello.sayHi("test")).isEqualTo("HI TEST");
         assertThat(proxiedHello.sayThankYou("test")).isEqualTo("THANK YOU TEST");
         assertThat(proxiedHello.pingpong("test")).isEqualTo("Pong test");
+    }
+
+    @DisplayName("ProxyFactoryBean으로 만들어진 프록시를 사용하여 say로 시작하는 메서드의 반환 값을 대문자로 변환한다.")
+    @Test
+    void sayHelloWithAopProxyFactory() {
+
+        HelloTarget proxiedHello = createProxiedHelloTarget();
+
+        assertThat(proxiedHello.sayHello("test")).isEqualTo("HELLO TEST");
+        assertThat(proxiedHello.sayHi("test")).isEqualTo("HI TEST");
+        assertThat(proxiedHello.sayThankYou("test")).isEqualTo("THANK YOU TEST");
+        assertThat(proxiedHello.pingpong("test")).isEqualTo("Pong test");
+    }
+
+    private HelloTarget createProxiedHelloTarget() {
+        return (HelloTarget) new ProxyFactoryBean(
+            new Advised(
+                HelloTarget.class,
+                new Advisor(( method ) -> method.getName().startsWith("say"), new UpperCaseAdvice())
+            )
+        ).getObject();
+    }
+
+
+    private static class UpperCaseAdvice implements AfterReturningAdvice {
+
+        @Override
+        public void invoke(MethodInvocation invocation) throws Throwable {
+            String result = (String) invocation.getReturnValue();
+            invocation.setReturnValue(result.toUpperCase());
+        }
     }
 }
