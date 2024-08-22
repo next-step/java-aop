d5af2899faf5907c21fad986c92850951a3b099b
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-08-22 13:29:22 +0900

    feat : advice의 실행조건을 책임지는 advisor 생성

---
+++ aop/src/main/java/com/interface21/framework/Advisor.java
@@ -0,0 +1,36 @@
+package com.interface21.framework;
+
+import java.lang.reflect.Method;
+
+public class Advisor {
+
+    private final PointCut pointCut;
+    private final Advice advice;
+
+    public Advisor(PointCut pointCut, Advice advice) {
+        this.pointCut = pointCut;
+        this.advice = advice;
+    }
+
+    public void invoke(MethodInvocation invocation) throws Throwable {
+        if (matches(invocation.getMethod())) {
+            advice.invoke(invocation);
+        }
+    }
+
+    public boolean isBeforeAdvice() {
+        return advice instanceof BeforeAdvice;
+    }
+
+    public boolean isAfterAdvice() {
+        return advice instanceof AfterAdvice;
+    }
+
+    public boolean isAfterReturningAdvice() {
+        return advice instanceof AfterReturningAdvice;
+    }
+
+    private boolean matches(Method method) {
+        return pointCut.matches(method);
+    }
+}
