96f204c7dae70765fc71c98c26f00df39aefcb4e
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-25 02:36:29 +0900

    chore: 코드 정리

--- aop/src/main/java/com/interface21/beans/factory/TrueMethodMatcher.java
+++ aop/src/main/java/com/interface21/beans/factory/TrueMethodMatcher.java
@@ -2,7 +2,7 @@ package com.interface21.beans.factory;
 
 import java.lang.reflect.Method;
 
-public class TrueMethodMatcher implements MethodMatcher{
+public class TrueMethodMatcher implements MethodMatcher {
 
     public static final TrueMethodMatcher INSTANCE = new TrueMethodMatcher();
 
--- di/src/test/java/samples/HelloTarget.java
+++ di/src/test/java/samples/HelloTarget.java
@@ -5,16 +5,4 @@ public class HelloTarget {
     public String sayHello(final String name) {
         return "Hello " + name;
     }
-
-    public String sayHi(final String name) {
-        return "Hi " + name;
-    }
-
-    public String sayThankYou(final String name) {
-        return "Thank You " + name;
-    }
-
-    public String pingpong(final String name) {
-        return "Pong " + name;
-    }
 }
