6344da84e8b7e81722ac1a4df54280cf286894d3
junseo <djawnstj44@naver.com>
2024-08-24 17:57:48 +0900

    test: ProxyFactoryBean 을 이용한 프록시 생성 테스트

--- di/src/test/java/com/interface21/context/support/AnnotationConfigWebApplicationContextTest.java
+++ di/src/test/java/com/interface21/context/support/AnnotationConfigWebApplicationContextTest.java
@@ -1,8 +1,12 @@
 package com.interface21.context.support;
 
 import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import samples.DatasourceConfiguration;
+import samples.ProxyConfiguration;
+import samples.SayCounter;
+import samples.TestService;
 
 import javax.sql.DataSource;
 
@@ -16,5 +20,19 @@ class AnnotationConfigWebApplicationContextTest {
         final var dataSource = applicationContext.getBean(DataSource.class);
         assertThat(dataSource).isInstanceOf(DataSource.class);
         assertThat(dataSource).isInstanceOf(JdbcDataSource.class);
+    }
+
+    @DisplayName("FactoryBean 으로 빈을 생성 한다")
+    @Test
+    public void factoryBeanConfiguration() throws Exception {
+        // given
+        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(ProxyConfiguration.class);
+        final TestService bean = applicationContext.getBean(TestService.class);
+
+        // when
+        bean.sayHello();
+
+        // then
+        assertThat(SayCounter.currentCount()).isEqualTo(1);
     }
 }
