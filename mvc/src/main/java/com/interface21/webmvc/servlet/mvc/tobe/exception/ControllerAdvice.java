665b3f07a787ae1a51503b6d69942509b55ed67a
jinyoung <jinyoungchoi95@gmail.com>
2024-08-25 04:31:07 +0900

    feat: annotation 스펙 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/exception/ControllerAdvice.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/exception/ControllerAdvice.java
@@ -1,4 +1,12 @@
 package com.interface21.webmvc.servlet.mvc.tobe.exception;
 
+import com.interface21.context.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
 public @interface ControllerAdvice {
 }
