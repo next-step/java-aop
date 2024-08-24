42304ad0026b14e614405385739b6a5a78fd3688
junseo <djawnstj44@naver.com>
2024-08-24 17:54:13 +0900

    refactor: CglibAopProxy 역할 변경

--- aop/src/main/java/com/interface21/framework/CglibAopProxy.java
+++ aop/src/main/java/com/interface21/framework/CglibAopProxy.java
@@ -1,11 +1,14 @@
 package com.interface21.framework;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+
 public class CglibAopProxy implements AopProxy {
 
     private final Object proxy;
 
-    public CglibAopProxy(final Object proxy) {
-        this.proxy = proxy;
+    public CglibAopProxy(final Class<?> targetClass, final MethodInterceptor methodInterceptor) {
+        this.proxy = Enhancer.create(targetClass, methodInterceptor);
     }
 
     @Override
