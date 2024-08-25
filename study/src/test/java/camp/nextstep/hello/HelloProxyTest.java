c69f419ec3f3acaadeb864ddb4925c9d6794d54c
Dongyoon Lee <nine.cow@gmail.com>
2024-08-25 22:49:07 +0900

    feat: cglib Proxy 적용

--- study/src/test/java/camp/nextstep/hello/HelloProxyTest.java
+++ study/src/test/java/camp/nextstep/hello/HelloProxyTest.java
@@ -1,5 +1,8 @@
 package camp.nextstep.hello;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.NoOp;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Proxy;
@@ -14,6 +17,24 @@ public class HelloProxyTest {
                 this.getClass().getClassLoader(),
                 new Class[]{Hello.class},
                 (proxy, method, args) -> ((String) method.invoke(hello, args)).toUpperCase());
+
+        assertThat(proxyInstance.sayHello("foo")).isEqualTo("HELLO FOO");
+        assertThat(proxyInstance.sayHi("bar")).isEqualTo("HI BAR");
+        assertThat(proxyInstance.sayThankYou("가나다")).isEqualTo("THANK YOU 가나다");
+    }
+
+    @Test
+    void cglibProxyTest() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(HelloTarget.class);
+        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+            if (method.getName().startsWith("say")) {
+                return ((String) proxy.invokeSuper(obj, args)).toUpperCase();
+            }
+            return NoOp.INSTANCE;
+        });
+        Object obj = enhancer.create();
+        HelloTarget proxyInstance = (HelloTarget) obj;
 
         assertThat(proxyInstance.sayHello("foo")).isEqualTo("HELLO FOO");
         assertThat(proxyInstance.sayHi("bar")).isEqualTo("HI BAR");
