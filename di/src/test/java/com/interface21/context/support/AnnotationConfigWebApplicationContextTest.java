c609bdc3fed418464ac09509e820f3ae9594355c
SeolYoungKim <nasur4da@gmail.com>
2024-08-28 00:17:20 +0900

    test(BeanFactory): @Transactional과 프록시에 대한 테스트 작성

--- di/src/test/java/com/interface21/context/support/AnnotationConfigWebApplicationContextTest.java
+++ di/src/test/java/com/interface21/context/support/AnnotationConfigWebApplicationContextTest.java
@@ -1,14 +1,18 @@
 package com.interface21.context.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import samples.DatasourceConfiguration;
-import samples.HelloFactoryBeanConfiguration;
 import samples.Hello;
+import samples.HelloFactoryBeanConfiguration;
+import samples.NoneTransactional;
+import samples.TransactionalOnClass;
+import samples.TransactionalOnMethod;
 import samples.componentscan.ComponentScanConfig;
 
 class AnnotationConfigWebApplicationContextTest {
@@ -39,5 +43,38 @@ class AnnotationConfigWebApplicationContextTest {
         Hello hello = applicationContext.getBean(Hello.class);
 
         assertThat(hello.getName()).isEqualTo("park");
+    }
+
+    @DisplayName("@Transactional 애노테이션이 클래스 수준에 달린 빈을 등록하면 프록시로 등록된다.")
+    @Test
+    void transactional() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(DatasourceConfiguration.class);
+
+        TransactionalOnClass transactionalOnClass = applicationContext.getBean(TransactionalOnClass.class);
+
+        assertThat(transactionalOnClass.getClass().getName()).contains("$$EnhancerByCGLIB$$");
+    }
+
+    @DisplayName("@Transactional 애노테이션이 메서드 수준에 달린 빈을 등록하면 프록시로 등록된다.")
+    @Test
+    void transactional2() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(DatasourceConfiguration.class);
+
+        TransactionalOnMethod transactionalOnMethod = applicationContext.getBean(TransactionalOnMethod.class);
+
+        assertThat(transactionalOnMethod.getClass().getName()).contains("$$EnhancerByCGLIB$$");
+    }
+
+    @DisplayName("@Transactional 애노테이션이 없는 빈은 프록시로 등록되지 않는다.")
+    @Test
+    void transactional3() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(DatasourceConfiguration.class);
+
+        NoneTransactional noneTransactional = applicationContext.getBean(NoneTransactional.class);
+
+        assertAll(
+                () -> assertThat(noneTransactional.getClass()).isEqualTo(NoneTransactional.class),
+                () -> assertThat(noneTransactional.getClass().getName()).isEqualTo("samples.NoneTransactional")
+        );
     }
 }
---
+++ di/src/test/java/samples/TransactionalOnMethod.java
@@ -0,0 +1,10 @@
+package samples;
+
+import com.interface21.transaction.annotation.Transactional;
+
+public class TransactionalOnMethod {
+    @Transactional
+    public void test() {
+
+    }
+}
