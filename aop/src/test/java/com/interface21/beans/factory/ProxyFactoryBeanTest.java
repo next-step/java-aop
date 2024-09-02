91a1d6870ce53fb7cd3e4953db136953ffb27fb8
Jivebreaddev <jivebreaddev@gmail.com>
2024-09-02 23:47:10 +0900

    test(ProxyFactoryBeanTest): ProxyBean으로 만든 객체에 대한 테스트를 진행합니다.

---
+++ aop/src/test/java/com/interface21/beans/factory/ProxyFactoryBeanTest.java
@@ -0,0 +1,38 @@
+package com.interface21.beans.factory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProxyFactoryBeanTest {
+
+    @Test
+    @DisplayName("Cglib 프록시 객체 생성")
+    void createProxyObject() {
+        ProxyFactoryBean<Sample> proxyFactoryBean = new ProxyFactoryBean<>(Sample.class,
+            new LogInterceptor());
+
+        Sample sample = proxyFactoryBean.getObject();
+
+        assertThat(sample).isInstanceOf(Sample.class);
+        assertThat(sample.hi()).isEqualTo("HI");
+        assertThat(Enhancer.isEnhanced(sample.getClass())).isTrue();
+    }
+
+
+    public class LogInterceptor implements MethodInterceptor {
+
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy)
+            throws Throwable {
+            Object returnValue = proxy.invokeSuper(obj, args);
+
+            return returnValue.toString().toUpperCase();
+        }
+    }
+}
---
+++ aop/src/test/java/com/interface21/beans/factory/Sample.java
@@ -0,0 +1,11 @@
+package com.interface21.beans.factory;
+
+public class Sample {
+
+    protected Sample() {
+    }
+
+    public String hi(){
+        return "hi";
+    }
+}
